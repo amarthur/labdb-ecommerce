3e0e7287b86c003ccd253dd3e034dbd683b114ee
Arthur <amarthur@usp.br>
2023-07-08 10:52:31 -0300

    Implement mongodb data mapper

--- data_mappers/map_mongo.py
+++ data_mappers/map_mongo.py
@@ -1,4 +1,27 @@
-import mongoengine
+from mongoengine import (DateTimeField, Document, IntField, ListField,
+                         ReferenceField, StringField)
+
+
+class User(Document):
+    _id = StringField(primary_key=True)
+
+
+class Rating(Document):
+    _id = IntField(primary_key=True)
+    user = ReferenceField(User, required=True, reverse_delete_rule=2)
+    rating = IntField(required=True)
+    date = DateTimeField(required=True)
+    comment = StringField()
+
+
+class Product(Document):
+    _id = IntField(primary_key=True)
+    ratings = ListField(ReferenceField(Rating, required=True, reverse_delete_rule=4))
+
+
+class Company(Document):
+    _id = StringField(primary_key=True)
+    ratings = ListField(ReferenceField(Rating, required=True, reverse_delete_rule=4))
 
 
 def main():
