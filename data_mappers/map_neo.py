842b29ab80fe231d5290ddaa31ec88cec84144d3
Arthur <amarthur@usp.br>
2023-07-10 18:23:14 -0300

    Implement method to get primary key name on neo mapper

--- data_mappers/map_neo.py
+++ data_mappers/map_neo.py
@@ -1,9 +1,18 @@
-from neomodel import (DateTimeProperty, IntegerProperty, RelationshipFrom,
-                      RelationshipTo, StringProperty, StructuredNode,
-                      StructuredRel, UniqueIdProperty, config, db)
+from neomodel import (IntegerProperty, RelationshipTo, StringProperty,
+                      StructuredNode)
 
 
-class User(StructuredNode):
+class PrimaryKey:
+
+    @classmethod
+    def get_pk(cls):
+        for name, prop in cls.defined_properties().items():
+            if prop.unique_index and prop.required:
+                return name
+        return None
+
+
+class User(StructuredNode, PrimaryKey):
     cpf = StringProperty(unique_index=True, required=True)
     name = StringProperty(required=True)
 
@@ -12,20 +21,20 @@ class User(StructuredNode):
     makes = RelationshipTo('Order', 'Makes')
 
 
-class Product(StructuredNode):
+class Product(StructuredNode, PrimaryKey):
     product_id = IntegerProperty(unique_index=True, required=True)
     name = StringProperty(required=True)
 
     belongs_to = RelationshipTo('Category', 'BelongsTo')
 
 
-class Order(StructuredNode):
+class Order(StructuredNode, PrimaryKey):
     order_id = IntegerProperty(unique_index=True, required=True)
 
     has_product = RelationshipTo('Product', 'HasProduct')
 
 
-class Category(StructuredNode):
+class Category(StructuredNode, PrimaryKey):
     category_id = IntegerProperty(unique_index=True, required=True)
     name = StringProperty(required=True)
 
