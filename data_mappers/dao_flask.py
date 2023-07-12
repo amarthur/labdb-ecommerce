d0d4512309fbee53e92251580f42e3edb051bf95
Arthur <amarthur@usp.br>
2023-07-12 19:25:16 -0300

    Implement basic api operations

--- data_mappers/dao_flask.py
+++ data_mappers/dao_flask.py
@@ -33,6 +33,21 @@ def get_mongo_repo():
     return get_repo('mongo_repo', 'mongo_url', repos.MongoRepository)
 
 
+def get_entity_dao(entity_class):
+    sql_repo = get_sql_repo()
+    neo_repo = get_neo_repo()
+    mongo_repo = get_mongo_repo()
+
+    if entity_class == "Carrier":
+        return daos.CarrierDAO(sql_repo)
+    elif entity_class == "Seller":
+        return daos.SellerDAO(sql_repo)
+    elif entity_class == "Product":
+        return daos.ProductDAO(sql_repo, neo_repo)
+    elif entity_class == "User":
+        return daos.UserDAO(sql_repo, neo_repo, mongo_repo)
+
+
 @app.route('/api/start', methods=['POST'])
 def start():
     get_sql_repo()
@@ -42,26 +57,33 @@ def start():
 
 @app.route('/api/create', methods=['POST'])
 def create():
-    sql_repo = get_sql_repo()
-    neo_repo = get_neo_repo()
-    mongo_repo = get_mongo_repo()
-
     data = request.get_json()
-    entity = data.get('entity')
+    entity_class = data.get('entity')
     entity_data = data.get('data')
 
-    if entity == "Carrier":
-        carrier_dao = daos.CarrierDAO(sql_repo)
-        carrier_dao.create(entity_data)
-    elif entity == "Seller":
-        seller_dao = daos.SellerDAO(sql_repo)
-        seller_dao.create(entity_data)
-    elif entity == "Product":
-        product_dao = daos.ProductDAO(sql_repo, neo_repo)
-        product_dao.create(entity_data)
-    elif entity == "User":
-        entity_dao = daos.UserDAO(sql_repo, neo_repo, mongo_repo)
-        entity_dao.create(entity_data)
+    entity_dao = get_entity_dao(entity_class)
+    entity_dao.create(entity_data)
+
+
+@app.route('/api/update', methods=['POST'])
+def update():
+    data = request.get_json()
+    entity_class = data.get('entity')
+    entity_id = data.get('entity_id')
+    entity_data = data.get('data')
+
+    entity_dao = get_entity_dao(entity_class)
+    entity_dao.update(entity_id, entity_data)
+
+
+@app.route('/api/delete', methods=['POST'])
+def delete():
+    data = request.get_json()
+    entity_class = data.get('entity')
+    entity_id = data.get('entity_id')
+
+    entity_dao = get_entity_dao(entity_class)
+    entity_dao.delete(entity_id)
 
 
 if __name__ == "__main__":
